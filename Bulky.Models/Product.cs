8ff88ee654c4aa7715c39299671e4286ff660210
kpetronikolos <kpetronikolos@gmail.com>
2023-09-20 22:36:32 +0200

    In the Product Create View display the Category List, using a ViewModel.

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -46,9 +47,12 @@ namespace BulkyBook.Models
           public double Price100 { get; set; }
 
           public int CategoryId { get; set; }
+
+          [ValidateNever]
           [ForeignKey("CategoryId")]
           public Category Category { get; set; }
 
+          [ValidateNever]
           public string ImageUrl { get; set; }
      }
 }
--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -16,32 +17,43 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
           }
           public IActionResult Index()
           {
-               List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
-
-               IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll()
-                    .Select(u => new SelectListItem
-                    {
-                         Text = u.Name,
-                         Value = u.Id.ToString()
-                    });   
+               List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();  
 
                return View(objProductList);
           }
 
           public IActionResult Create()
           {
-               return View();
+               ProductVM productVM = new()
+               {
+                    CategoryList =  _unitOfWork.Category.GetAll()
+                    .Select(u => new SelectListItem
+                    {
+                         Text = u.Name,
+                         Value = u.Id.ToString()
+                    }),
+                    Product = new Product()
+               };
+
+               return View(productVM);
           }
 
           [HttpPost]
-          public IActionResult Create(Product product)
+          public IActionResult Create(ProductVM productVM)
           {
                if (!ModelState.IsValid)
                {
-                    return View();
+                    productVM.CategoryList = _unitOfWork.Category.GetAll()
+                    .Select(u => new SelectListItem
+                    {
+                         Text = u.Name,
+                         Value = u.Id.ToString()
+                    });
+
+                    return View(productVM);
                }
 
-               _unitOfWork.Product.Add(product);
+               _unitOfWork.Product.Add(productVM.Product);
                _unitOfWork.Save();
                TempData["success"] = "Product created successfully";
                return RedirectToAction("Index");
