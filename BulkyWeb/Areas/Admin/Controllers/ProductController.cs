69d6b94385d467116d067aea0db7b112aeb36484
kpetronikolos <kpetronikolos@gmail.com>
2023-09-20 23:10:51 +0200

    Combine Product Create and Edit Pages.

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -22,7 +22,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                return View(objProductList);
           }
 
-          public IActionResult Create()
+          public IActionResult Upsert(int? id)
           {
                ProductVM productVM = new()
                {
@@ -35,11 +35,13 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                     Product = new Product()
                };
 
+               if (id > 0) productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+
                return View(productVM);
           }
 
           [HttpPost]
-          public IActionResult Create(ProductVM productVM)
+          public IActionResult Upsert(ProductVM productVM, IFormFile? file)
           {
                if (!ModelState.IsValid)
                {
@@ -57,38 +59,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                _unitOfWork.Save();
                TempData["success"] = "Product created successfully";
                return RedirectToAction("Index");
-          }
-
-          public IActionResult Edit(int? id)
-          {
-               if (id == null || id == 0)
-               {
-                    return NotFound();
-               }
-
-               var product = _unitOfWork.Product.Get(u => u.Id == id);
-
-               if (product == null)
-               {
-                    return NotFound();
-               }
-
-               return View(product);
-          }
-
-          [HttpPost]
-          public IActionResult Edit(Product product)
-          {
-               if (!ModelState.IsValid)
-               {
-                    return View();
-               }
-
-               _unitOfWork.Product.Update(product);
-               _unitOfWork.Save();
-               TempData["success"] = "Product updated successfully";
-               return RedirectToAction("Index");
-          }
+          }          
 
           public IActionResult Delete(int? id)
           {
