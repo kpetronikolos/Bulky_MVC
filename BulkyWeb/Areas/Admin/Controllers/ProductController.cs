316142d32e6cdc905badc473c48392334f5c96c8
kpetronikolos <kpetronikolos@gmail.com>
2023-09-21 15:03:06 +0200

    Fix issue when updating a product.

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -82,7 +82,15 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                     productVM.Product.ImageUrl = @$"\images\product\{filename}";
                }
 
-               _unitOfWork.Product.Add(productVM.Product);
+               if (productVM.Product.Id == 0)
+               {
+                    _unitOfWork.Product.Add(productVM.Product);
+               }
+               else
+               {
+                    _unitOfWork.Product.Update(productVM.Product);
+               }
+               
                _unitOfWork.Save();
                TempData["success"] = "Product created successfully";
                return RedirectToAction("Index");
