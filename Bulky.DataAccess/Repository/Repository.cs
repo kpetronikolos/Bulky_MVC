d522fa5b8d72128860c6127cba21de3b5e5328df
kpetronikolos <kpetronikolos@gmail.com>
2023-09-21 15:43:04 +0200

    Loading Navigation Properties

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -21,16 +21,36 @@ namespace BulkyBook.DataAccess.Repository
                dbSet.Add(entity);
           }
 
-          public T Get(Expression<Func<T, bool>> filter)
+          public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
           {
                IQueryable<T> query = dbSet;
                query = query.Where(filter);
+
+               if (!String.IsNullOrEmpty(includeProperties))
+               {
+                    foreach (var includeProp in includeProperties
+                         .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                         query = query.Include(includeProp);
+                    }
+               }
+
                return query.FirstOrDefault();
           }
 
-          public IEnumerable<T> GetAll()
+          public IEnumerable<T> GetAll(string? includeProperties = null)
           {
                IQueryable<T> query = dbSet;
+
+               if (!String.IsNullOrEmpty(includeProperties))
+               {
+                    foreach (var includeProp in includeProperties
+                         .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                         query = query.Include(includeProp);
+                    }
+               }
+
                return query.ToList();
           }
 
