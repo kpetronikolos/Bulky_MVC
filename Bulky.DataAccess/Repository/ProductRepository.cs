060b4b6805d0165a88c78c6f20d4a6557a5b22a3
kpetronikolos <kpetronikolos@gmail.com>
2023-09-21 15:18:34 +0200

    Update Product Custome Code

--- Bulky.DataAccess/Repository/ProductRepository.cs
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -15,7 +15,26 @@ namespace BulkyBook.DataAccess.Repository
 
           public void Update(Product product)
           {
-               _db.Products.Update(product);
+               var objFromDb = _db.Products.FirstOrDefault(u => u.Id == product.Id);
+
+               if (objFromDb != null)
+               {
+                    objFromDb.Title = product.Title;
+                    objFromDb.ISBN = product.ISBN;
+                    objFromDb.Price = product.Price;
+                    objFromDb.Price50 = product.Price50;
+                    objFromDb.ListPrice = product.ListPrice;
+                    objFromDb.Price100 = product.Price100;
+                    objFromDb.Description = product.Description;
+                    objFromDb.CategoryId = product.CategoryId;
+                    objFromDb.Author = product.Author;
+
+                    if (objFromDb.ImageUrl != null)
+                    {
+                         objFromDb.ImageUrl = product.ImageUrl;
+                    }
+                    
+               }
           }
      }
 }
