d39ac681ed7f4f79b3f50bd506f5816c8a584b5b
kpetronikolos <kpetronikolos@gmail.com>
2023-09-23 12:37:57 +0200

    Add Razor Pages configuration to fix the issue with the Register and Login Page.

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
@@ -32,6 +32,8 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
