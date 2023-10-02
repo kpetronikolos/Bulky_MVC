11a4127f95ab431b55cd0533c2f2be58f7555e77
kpetronikolos <kpetronikolos@gmail.com>
2023-10-02 16:31:02 +0200

    Change the model of the Details Pages to ShoppingCart.

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -25,8 +25,14 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 
           public IActionResult Details(int productId)
           {
-               Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
-               return View(product);
+               ShoppingCart shoppingCart = new()
+               {
+                    Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+                    Count = 1,
+                    ProductId = productId
+               };
+
+               return View(shoppingCart);
           }
 
           public IActionResult Privacy()
