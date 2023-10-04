eec60577c4420d137e51234fe6a2a139c5dcc3ae
kpetronikolos <kpetronikolos@gmail.com>
2023-10-04 21:54:52 +0200

    Add Shopping Cart UI

---
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+     public class CartController : Controller
+     {
+          [Area("customer")]
+          public IActionResult Index()
+          {
+               return View();
+          }
+     }
+}
