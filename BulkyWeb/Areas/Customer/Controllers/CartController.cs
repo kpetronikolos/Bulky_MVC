86f70d5aa4b682072f6e5a8fadc8720cdf8b5425
kpetronikolos <kpetronikolos@gmail.com>
2023-10-05 14:53:41 +0200

    Update Quantity in Shopping Cart.

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -7,6 +7,8 @@ using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
+     [Area("customer")]
+     [Authorize]
      public class CartController : Controller
      {
           private readonly IUnitOfWork _unitOfWork;
@@ -16,9 +18,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
           {
                _unitOfWork = unitOfWork;
           }
-
-          [Area("customer")]
-          [Authorize]
+          
           public IActionResult Index()
           {
                var claimsIdentity = (ClaimsIdentity)User.Identity;
@@ -36,6 +36,46 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                }
 
                return View(ShoppingCartVM);
+          }
+
+          public IActionResult Plus(int cartId)
+          {
+               var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+               cartFromDb.Count += 1;
+
+               _unitOfWork.ShoppingCart.Update(cartFromDb);
+               _unitOfWork.Save();
+
+               return RedirectToAction(nameof(Index));
+          }
+
+          public IActionResult Minus(int cartId)
+          {
+               var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+
+               if (cartFromDb.Count <= 1)
+               {
+                    _unitOfWork.ShoppingCart.Remove(cartFromDb);
+               }
+               else
+               {
+                    cartFromDb.Count -= 1;
+                    _unitOfWork.ShoppingCart.Update(cartFromDb);
+               }
+
+               _unitOfWork.Save();
+
+               return RedirectToAction(nameof(Index));
+          }
+
+          public IActionResult Remove(int cartId)
+          {
+               var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+
+               _unitOfWork.ShoppingCart.Remove(cartFromDb);
+               _unitOfWork.Save();
+
+               return RedirectToAction(nameof(Index));
           }
 
           private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
