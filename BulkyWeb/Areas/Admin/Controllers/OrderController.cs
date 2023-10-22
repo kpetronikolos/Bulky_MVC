e077237b2dbadbac1a5106bb0d6dc9974dcf8603
kpetronikolos <kpetronikolos@gmail.com>
2023-10-22 15:18:03 +0200

    Only Admin and Employee can see all the orders

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -5,11 +5,11 @@ using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
 	[Area("Admin")]
-     [Authorize(Roles = SD.Role_Admin)]
      public class OrderController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
@@ -70,8 +70,20 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
           [HttpGet]
 		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> objOrderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			List<OrderHeader> objOrderHeaderList;
 
+               if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+               {
+                    objOrderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+               }
+               else
+               {
+                    var claimsIdentity = (ClaimsIdentity)User.Identity;
+                    var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                    objOrderHeaderList = _unitOfWork.OrderHeader
+                        .GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser").ToList();
+               }
 
                switch (status)
                {
